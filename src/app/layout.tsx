82ae93fbca6ec0722fea78ae1a5fe565031d1b3b
kumod-12 <kumodmehta333@gmail.com>
2025-08-18 00:15:49 +0530

    Add files via upload
    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import ThemeProvider from '@/components/ThemeProvider';
 import "./globals.css";
 
@@ -124,6 +125,7 @@ export default function RootLayout({
       >
         <ThemeProvider />
         {children}
+        <Analytics />
       </body>
     </html>
   );
